70c7ca070b1163008cbddb1b82064019526e442f
anaf08 <andradeanaflavia95@gmail.com>
2022-05-23 18:54:22 -0300

    salvar

--- rel05/sd.R
+++ rel05/sd.R
@@ -1,18 +1,41 @@
 # Calculando o devsio padrão no leem
 
-sdev <- function (x, rounding = 2, na.rm = FALSE, details = FALSE, grouped = TRUE) 
-{
-  if (class(x) != "leem") 
-    stop("Use the 'new_leem()' function to create an object of class leem!", 
-         call. = FALSE)
-  if (class(x) == "leem" & is.null(attr(x, "table"))) 
-    x <- tabfreq(x)
+sdev <- function(x, rounding = 2, na.rm = FALSE, details = FALSE,
+                 grouped = TRUE) {
+  if (class(x) != "leem") stop("Use the 'new_leem()' function to create an object of class leem!", call. = FALSE)
+  if (class(x) == "leem" & is.null(attr(x, "table"))) x <- tabfreq(x)
   if (attr(x, "variable") == "discrete") {
     numchar <- is.numeric(x$estat$raw_data)
     if (numchar) {
-      desvpad <- round(sd(x = x$estat$raw_data, na.rm = na.rm), 
+      desvpad <- round(sd(x = x$estat$raw_data,
+                          na.rm = na.rm), digits = rounding)
+      resume <- list(sdeviation = desvpad, table = x$tabela, rawdata = x$estat$raw_data)
+      if (details) {
+        return(resume)
+      } else {
+        return(desvpad)
+      }
+      
+    } else {
+      stop("Measure not used for this data type!", call. = FALSE,
+           domain = "R-leem")
+    }
+  }
+  if (attr(x, "variable") == "continuous") {
+    if (grouped == TRUE) {
+      desvpad <- sqrt((variance(x)))
+      resume <- list(sdeviation = desvpad, table = x$tabela,
+                     rawdata = x$estat$raw_data)
+      if (details) {
+        return(resume)
+      }
+      else {
+        return(desvpad)
+      }
+    } else {
+      desvpad <- round(sd(x = x$estat$raw_data, na.rm = na.rm),
                        digits = rounding)
-      resume <- list(sdeviation = desvpad, table = x$tabela, 
+      resume <- list(sdeviation = desvpad, table = x$tabela,
                      rawdata = x$estat$raw_data)
       if (details) {
         return(resume)
@@ -21,41 +44,10 @@ sdev <- function (x, rounding = 2, na.rm = FALSE, details = FALSE, grouped = TRU
         return(desvpad)
       }
     }
-    else {
-      stop("Measure not used for this data type!", 
-           call. = FALSE, domain = "R-leem")
-    }
   }
-  if (attr(x, "variable") == "continuous") {
-    if (grouped == TRUE){
-      
-    }
-    desvpad <- sqrt((variance(x)))
-    resume <- list(sdeviation = desvpad, table = x$tabela, 
-                   rawdata = x$estat$raw_data)
-    if (details) {
-      return(resume)
-    }
-    else {
-      return(desvpad)
-    } 
-  } else {
-    desvpad <- round(sd(x = x$estat$raw_data, na.rm = na.rm), 
-                     digits = rounding)
-    resume <- list(sdeviation = desvpad, table = x$tabela, 
-                   rawdata = x$estat$raw_data)
-    if (details) {
-      return(resume)
-    }
-    else {
-      return(desvpad)
-    }
-  }
-  
 }
 
 set.seed(10)
-
 x <- rnorm(36, 100, 50)
 x <- new_leem(x, variable = 2)
 sdev(x)
