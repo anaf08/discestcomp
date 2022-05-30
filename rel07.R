edc22971bed2c4ede629bc60a706a18421c0cbc6
anaf08 <andradeanaflavia95@gmail.com>
2022-05-30 20:45:46 -0300

    salvar

---
+++ rel07.R
@@ -0,0 +1,21 @@
+#Probability
+
+p <- function(q, dist = "binomial", lower.tail = TRUE, rounding = 4, porcentage = FALSE, ...){
+  if(dist == "binomial"){
+argnames <- names(formals(p))
+  # if(!any(argnames == "size")) stop("Insira argumento 'size'!", call. = FALSE)
+    # if(!any(argnames == "prob")) stop("Insira argumento 'prob'!", call. = FALSE)
+     if (lower.tail){
+       prob <- pbinom(q = x, size = size, )
+     }
+  }
+  prob <- round(prob, rounding)
+  if (porcentage == TRUE) prob <- prob * 100
+  return(prob)
+    
+}
+
+ 
+ 
+ 
+ 
