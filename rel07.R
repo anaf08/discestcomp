4e6ae50d43d2452abf312adbc38df5e9755a0bd8
anaf08 <andradeanaflavia95@gmail.com>
2022-06-01 08:37:12 -0300

    salvar

--- rel07.R
+++ rel07.R
@@ -1,6 +1,6 @@
 #Probability
 
-p <- function(q, dist = "binomial", lower.tail = TRUE, rounding = 4, porcentage = FALSE, ...){
+p <- function(q, dist = "binomial", lower.tail = TRUE, rounding = 4, porcentage = FALSE, gui = "plot", ...){
   if(dist == "binomial"){
 argnames <- names(formals(p))
   # if(!any(argnames == "size")) stop("Insira argumento 'size'!", call. = FALSE)
