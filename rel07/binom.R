62c58f4c269d5cef03e02df3b203bbcd1ada7741
anaf08 <andradeanaflavia95@gmail.com>
2022-06-23 13:58:11 -0300

    salvo

--- rel07/binom.R
+++ rel07/binom.R
@@ -1,7 +1,7 @@
 # Distribuição Binomial
 
 p <- function(q, dist = "binomial", lower.tail = TRUE, rounding = 4, porcentage = FALSE, gui = "plot", ...) {
-  argaddit$size <- list(...)
+  argaddit <- list(...)
   argdef <- formals(p)
   if (dist == "binomial") {
     if (!any(names(argaddit) == "size")) stop("Insira o argumento 'size'!", call. = FALSE)
@@ -51,39 +51,6 @@ p <- function(q, dist = "binomial", lower.tail = TRUE, rounding = 4, porcentage 
         legend("topleft", bty="n", fill="red",
                legend=substitute(P(X<=q)==Pr~"\n\n"~size == n~p==prob , list(q=qq, Pr=Pr, n = size, prob = prob)))
       }
-
-      if (gui == "rstudio") {
-
-        manipulate::manipulate(plotcurve(size, prob),
-                               size = manipulate::slider(-6, 6, q),
-                               prob = manipulate::slider(mu, mu + 200, mu))
-      }
-
-    } else{
-      plotcurve <- function(q, size, prob) {
-        curve(dbinom(x, size = , prob =sigma), -6, 6, ylab = expression(f[T](t)), xlab="T")
-        x <- seq(q, 6, by=0.01)
-        y <- seq(-6, q, by=0.01)
-        fx <- dbinom(x, size = mu, prob = sigma)
-        fy <- dbinom(y, size = mu, prob = sigma)
-        polygon(c(x, rev(x)),
-                c(fx, rep(0, length(fx))),
-                col="red")
-        polygon(c(y, rev(y)),
-                c(fy, rep(0, length(fy))),
-                col="gray90")
-        abline(v=0, lty=2)
-        qq <- round(q, digits=2)
-        qqaux <-round(q, digits=2)
-        Pr <- round(pbinom(qq, size = mu, prob =sigma, lower.tail = F), digits=rounding)
-        Pr <- gsub("\\.", ",", Pr)
-        qq <- gsub("\\.", ",", qq)
-        axis(side=1, at=qqaux, labels=qqaux,
-             col="red", font = 2)
-        abline(v = qqaux, lty=2, col = "red")
-        legend("topleft", bty="n", fill="red",
-               legend=substitute(P(X~`>`~q)==Pr~"\n\n"~size==mu, list(q=qq, Pr=Pr, mu = mu)))
-      }
       if (gui == "plot"){
         # Probability
         size <- argaddit$size
@@ -93,12 +60,75 @@ p <- function(q, dist = "binomial", lower.tail = TRUE, rounding = 4, porcentage 
         plotcurve(q, size, prob = sucesso)
       }
       if (gui == "rstudio") {
-        # Probability
         size <- argaddit$size
         sucesso <- argaddit$prob
         prob <- pbinom(q = q, size = size, prob = sucesso)
         manipulate::manipulate(plotcurve(q, size, prob),
                                q = manipulate::slider(0, size, q),
+                               size = manipulate::slider(size, size + 30, size),
+                               prob = manipulate::slider(sucesso, 1, sucesso))
+      }
+
+    } else{
+      plotcurve <- function(q, size, prob){
+        rmin <- size * prob - 4 * sqrt(size * prob*(1 - prob))
+        if (rmin < 0) rmin < 0 else rmin <- round(rmin)
+        x <- rmin:size
+        x1 <- rmin:q
+        x2 <- (q + 1):size
+        probx <- dbinom(x, size = size, prob = prob)
+        probx1 <- dbinom(x1, size = size, prob = prob)
+        probx2 <- dbinom(x2, size = size, prob = prob)
+        
+        # Area de plotagem
+        xlim <- c(rmin, size)
+        ylim <- c(min(probx), max(probx) + 0.1)
+        
+        # Area de plotagem
+        plot.new()
+        plot.window(xlim, ylim)
+        
+        # Eixos
+        axis(1)
+        axis(2)
+        
+        # Titulo e lables
+        title(ylab = expression(p[X](x)), xlab="X")
+        
+        lines (x1, probx1, type = "h", panel.first = grid(),lwd = 2)
+        points(x1, probx1, lwd = 2, pch = 19)
+        
+        lines (x2, probx2, type = "h",lwd = 2, col = "red")
+        points(x2, probx2, lwd = 2, pch = 19, col = "red")
+        
+        
+        abline(v= size*prob, lty=2)
+        qq <- round(q, digits=2)
+        qqaux <- round(q, digits=2)
+        Pr <- round(pbinom(q, size = size, prob = prob, lower.tail = F), rounding)
+        Pr <- gsub("\\.", ",", Pr)
+        qq <- gsub("\\.", ",", qq)
+        axis(side=1, at=qqaux, labels=qqaux,
+             col="red", font = 2)
+        abline(v = qqaux, lty=2, col = "red")
+        legend("topleft", bty="n", fill="red",
+               legend=substitute(P(X>q)==Pr~"\n\n"~size == n~p==prob , list(q=qq, Pr=Pr, n = size, prob = prob)))
+      }
+      if (gui == "plot"){
+        # Probability
+        size <- argaddit$size
+        sucesso <- argaddit$prob
+        prob <- pbinom(q = q, size = size, prob = sucesso,lower.tail = FALSE)
+        # Plot
+        plotcurve(q, size, prob = sucesso)
+      }
+      if (gui == "rstudio") {
+        # Probability
+        size <- argaddit$size
+        sucesso <- argaddit$prob
+        prob <- pbinom(q = q, size = size, prob = sucesso, lower.tail = FALSE)
+        manipulate::manipulate(plotcurve(q, size, prob),
+                               q = manipulate::slider(q, size, q),
                                size = manipulate::slider(size, size + 30, size),
                                prob = manipulate::slider(sucesso, 1, sucesso)
                                )
